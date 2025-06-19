40fc3e3c3cddfb32f75584806c661944a3bf8fd4
Thorfilo2612 <ando1661@gmail.com>
2025-06-19 16:53:02 -0400

    Paginas de login y registro

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="page">
+      <h2>Iniciar Sesión</h2>
+      {/* Aquí ira elformulario de login */}
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div className="page">
+      <h2>Registrarse</h2>
+      {/* Aquí ira el formulario de registro */}
+    </div>
+  );
+};
+
+export default RegisterPage;
