6dbedb065d5df586e8446a5549a4a2f3bed574c3
Thorfilo2612 <ando1661@gmail.com>
2025-06-19 17:31:34 -0400

    Update App.jsx
    Actualizacion de app.jsx para el uso del react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 import "./styles/index.css";
 import "./styles/App.css";
 
-// Componentes
+// Componentes comunes
 import Header from "./components/Header/Header";
+
+// Páginas
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+// Secciones de Landing
 import LandingCTA from "./components/LandingCTA/LandingCTA";
 import Features from "./components/Features/Features";
 import Testimonials from "./components/Testimonials/Testimonials";
@@ -18,15 +25,26 @@ function App() {
     AOS.init({ duration: 1000 });
   }, []);
 
+  // Subcomponente para mostrar toda la landing
+  const FullLanding = () => (
+    <>
+      <LandingPage />
+      <LandingCTA />
+      <Features />
+      <Testimonials />
+      <FAQAndCTA />
+    </>
+  );
+
   return (
     <div className="App">
       <Header />
       <main>
-        <LandingPage />
-        <LandingCTA />
-        <Features />
-        <Testimonials />
-        <FAQAndCTA />
+        <Routes>
+          <Route path="/" element={<FullLanding />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
       </main>
     </div>
   );
