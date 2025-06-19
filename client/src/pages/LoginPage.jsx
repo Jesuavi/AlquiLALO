22b39cfdac480e5f9bd399175c364cd29315ad7b
Thorfilo2612 <ando1661@gmail.com>
2025-06-19 17:32:39 -0400

    Update LoginPage.jsx
    Mejora, Login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Para el botón de Registrarse
+import "./LoginPage.css";
 
 const LoginPage = () => {
   return (
-    <div className="page">
-      <h2>Iniciar Sesión</h2>
-      {/* Aquí ira elformulario de login */}
+    <div className="login-page">
+      <div className="login-container">
+        <h1>INICIO DE SESIÓN</h1> {/* Título principal */}
+        
+        <form className="login-form">
+          <div className="form-group">
+            <label>Email</label>
+            <input 
+              type="email" 
+              placeholder="ejemplo@unimet.edu.ve" 
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Contraseña</label>
+            <input 
+              type="password" 
+              placeholder="••••••" 
+            />
+          </div>
+
+          <button type="submit" className="login-button">
+            Iniciar Sesión
+          </button>
+        </form>
+
+        <div className="register-prompt">
+          <p>¿Aún no tienes cuenta? Regístrate en el botón de abajo</p>
+          <Link to="/register" className="register-button">
+            Registrarse
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
