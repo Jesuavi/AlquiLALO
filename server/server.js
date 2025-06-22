d078000aca1f646619aeae3ecb4f01929015221b
fermb02 <155971382+fermb02@users.noreply.github.com>
2025-06-21 20:38:29 -0400

    Update server.js
    agregar ruta

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
-
+const espaciosRoutes = require("./routes/espacios");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
